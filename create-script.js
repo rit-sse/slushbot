60f31f103e28d92a06fbae7415c4db2d07e75a36
Kristen Mills <kristen@kristen-mills.com>
2016-02-05 15:46:39 -0800

    Added a command for creating a new command file

---
+++ create-script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import { argv } from 'yargs';
+import fs from 'fs';
+
+if (!argv.name || !(argv.type === 'bot' || argv.type === 'slash')) {
+  console.log('Name and type are required');
+} else {
+  const filePath = `src/matchers/${argv.type}/${argv.name.replace(' ', '-')}.js`;
+  fs.stat(filePath, err => {
+    if (err) {
+      fs.readFile(`templates/${argv.type}.js.template`, 'utf-8', (e, data) => {
+        if (e) {
+          console.log(err);
+        } else {
+          const replaced = data.replace('%NAME%', argv.name);
+          fs.writeFile(filePath, replaced, error => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(filePath, 'created!');
+            }
+          });
+        }
+      });
+    } else {
+      console.log('A script of that name and type already exists');
+    }
+  });
+}
