1307dc4841f681ab4e5a5575d5dbbf425ba353a2
Kristen Mills <kristen@kristen-mills.com>
2016-02-06 19:30:08 -0800

    Whoops

--- api.js
+++ api.js
@@ -3,8 +3,8 @@
 import API from 'sse-api-client';
 import nconf from './config';
 
-if (!nconf.get('API_ROOT') || !nconf.get('SLACK_SECRET')) {
+if (!nconf.get('SSE_API_ROOT') || !nconf.get('SLACK_SECRET')) {
   console.log('Warning: API_ROOT or SLACK_SECRET not set');
 }
 
-export default () => new API(nconf.get('API_ROOT'));
+export default () => new API(nconf.get('SSE_API_ROOT'));
