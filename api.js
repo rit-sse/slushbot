5d4dda19186289a5402e9f8c3acbcb7a20e721eb
Kristen Mills <kristen@kristen-mills.com>
2016-02-06 16:44:24 -0800

    A bit of re org and go works. fixes #7

---
+++ api.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import API from 'sse-api-client';
+import nconf from './config';
+
+if (!nconf.get('API_ROOT') || !nconf.get('SLACK_SECRET')) {
+  console.log('Warning: API_ROOT or SLACK_SECRET not set');
+}
+
+export default () => new API(nconf.get('API_ROOT'));
