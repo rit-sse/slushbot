28517db68e9c57723ff221399f1fb2bc9291e605
Michael Timbrook <timbrook480@gmail.com>
2017-01-25 18:13:46 -0500

    Trying to use api

--- notifications/index.js
+++ notifications/index.js
@@ -1,6 +1,18 @@
-export default function registerNotifications(slushbot) {
-  slushbot.say({
-    text: 'Hello guys :)',
-    channel: 'sse-tech',
-  });
+import fetch from 'node-fetch';
+import nconf from '../config';
+
+export default function registerNotifications() {
+  fetch('https://slack.com/api/chat.postMessage', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: nconf.get('SLACK_BOT_TOKEN'),
+      channel: 'sse-tech',
+      text: 'Hi guys, I\'m alive',
+    }),
+  })
+  .then(() => console.log('done'))
+  .catch(err => console.log(err));
 }
