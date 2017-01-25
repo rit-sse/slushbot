10dc42c93cd95a7780a82e36b48431e2899c541d
Michael Timbrook <timbrook480@gmail.com>
2017-01-25 18:57:14 -0500

    As user

--- notifications/index.js
+++ notifications/index.js
@@ -3,6 +3,7 @@ export default function registerNotifications(slushbot) {
   slushbot.api.chat.postMessage({
     text: 'Did it work?',
     channel: 'sse-tech',
+    as_user: true
   }, (err, res) => {
     if (!err) {
       console.log(res);
