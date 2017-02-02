a0f0100964d55b8542a1837e310d0dab1a57d2ef
Michael Timbrook <timbrook480@gmail.com>
2017-02-02 18:01:41 -0500

    Dumb

--- matchers/slash/events.js
+++ matchers/slash/events.js
@@ -16,6 +16,7 @@ const matcher = {
     if (matchCommand) {
       client.set('events::' + message.channel, true);
       bot.replyPublic(message, 'Adding ' + message.channel + ' to event creation notifications');
+    }
   },
 };
 
